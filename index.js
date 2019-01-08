40cf1275e44bb45f6cb954740da5b7a44edd7977
Rishabh Rawat <30312043+Showtim3@users.noreply.github.com>
2019-01-08 22:51:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+$(document).ready(function(){
+
+    const url='https://api.myjson.com/bins/blxqf';
+    
+    var question=$('#question');
+    var nextButton=$("#next");
+    var submitButton=$("#submit");
+    var toggle=$(".toggle");
+    var beginButton=$("#begin");
+
+    
+    var arr=[];  //array to store outputs 
+    var timeStamp=[];
+    var optionSelected=[];  
+    var response; //global variable to store data returned through ajax call
+    var beginClicked;
+    var bool=false; 
+    var ques=0; //keeping track of 'id'
+
+
+    beginButton.click(function()
+    {   console.log("CLick on begin detected");
+        beginClicked=true;
+        $("#intro").remove();
+        handle();
+        
+    })
+
+
+    //the function where actual ajax call is happening
+    function call()
+    {
+        $.ajax({
+        url:url,
+        method:'get',
+        success:function(data)
+            {   console.log("Call worked");
+            store(data);
+            }
+        })
+    }
+
+    
+function store(data)
+{   if(bool==false)
+    {
+        response=data;
+        bool=true;
+    }
+
+}
+
+
+function handle(){
+    
+    timeStamp.push($.now());
+    question.append('Q'+response.questions[ques].id+') '+response.questions[ques].statment);
+    toggle.css("display","block");
+    ques++;
+   
+}
+
+
+submitButton.click(function(){
+  
+    timeStamp.push($.now());
+    $.each($("input[name='options']:checked"), function(){            
+        optionSelected.push($(this).val());
+    });
+  
+    alert(optionSelected[ques-1]+" submitted successfully.");
+  
+});
+
+
+nextButton.click(function(){
+    if(ques<=2)
+    {
+        question.empty();
+        $('input[name=options]').prop('checked',false);
+        handle();
+    }
+    else 
+    {
+        for(i=0;i<3;i++)
+        {      
+        var container ={id : i+1,timeTaken:((timeStamp[i+1]-timeStamp[i])/1000),option:optionSelected[i]};
+        arr.push(container);
+        }
+
+        var obj1={answers:arr};
+        var finalAns=JSON.stringify(obj1);
+        
+        $('#quiz').remove();
+        $('#output').append("OUTPUT :");
+        $('#final').append(finalAns);
+    }
+    
+
+});
+
+call();
+});
